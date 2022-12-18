034eb8d39e0dc133464f353ffae9c7d366893d21
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-12-18 11:54:33 -0100

    feat: font-page layout

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,22 @@
 
 import './Home.css'
+
+import image from '../../assets/image1.jpg'
 const Home = () =>{
     return(
         <div className="home">
-            <div className="left">
-                <p>text</p>
+            <div className="about-left">
+                <h1 className='about-title'>Ocean Vision</h1>
+                <div className="about-description">
+                    <p>orci rutrum ullamcorper. Fusce sagittis maximus convallis. Donec at elit condimentum, aliquet sem ut, pretium orci. Cras at facilisis magna. Maecenas sapien feugiat fermentum ut.</p>
+                    <p>orci rutrum ullamcorper. Fusce sagittis maximus convallis. Donec at elit condimentum, aliquet sem ut, pretium orci. Cras at facilisis magna. Maecenas sapien feugiat fermentum ut.</p>
+                    <p>orci rutrum ullamcorper. Fusce sagittis maximus convallis. Donec at elit condimentum, aliquet sem ut, pretium orci. Cras at facilisis magna. Maecenas sapien feugiat fermentum ut.</p>
+                </div>
             </div>
-            <div className="right">
-                <p>Image</p>
+            <div className="about-right">
+                <div className="image-container">
+                    <img src={image} alt="description"/>
+                </div>
             </div>
         </div>
     )
