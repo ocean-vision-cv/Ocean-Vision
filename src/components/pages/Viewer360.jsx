fdaf794840811909ecdabb5f7b18b6593469446b
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-01-02 19:06:55 -0100

    fix slidebutton not visible on mobile

--- src/components/pages/Viewer360.jsx
+++ src/components/pages/Viewer360.jsx
@@ -1,4 +1,4 @@
-import Icon360 from '../layout/Icon360';
+// import Icon360 from '../layout/Icon360';
 import Quote from '../layout/Quote';
 import SlideButton from '../layout/SlideButton';
 import './Viewer360.css'
@@ -12,7 +12,7 @@ const Viewer360 = () =>{
             <SlideButton/>
     
             <iframe className='frame' frameborder="0" title='360image' src='https://momento360.com/e/u/549c51676a5b4e1a9d09741c05f740ae?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75&size=medium&display-plan=true'/>
-            <p className='icon'><Icon360/></p>
+            {/* <p className='icon'><Icon360/></p> */}
         </section>
     )
 };
