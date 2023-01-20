5d1369414dcaa68fea41895e0bfabd20cfb8465f
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-01-20 14:22:47 -0100

    feat: add events section

---
+++ src/components/layout/Images.jsx
@@ -0,0 +1,25 @@
+
+import galleryHackthon1 from '../../assets/images/gallery/gallery-hackthon1.jpg'
+import galleryHackthon2 from '../../assets/images/gallery/gallery-hackthon2.jpg'
+import galleryHackthon3 from '../../assets/images/gallery/gallery-hackthon3.jpg'
+import galleryImage1 from  '../../assets/images/gallery/gallery-image1.jpg'
+import galleryImage2 from '../../assets/images/gallery/gallery-image2.jpg'
+import galleryImage3 from '../../assets/images/gallery/gallery-image3.jpg'
+import galleryImage4 from '../../assets/images/gallery/gallery-image4.jpg'
+import galleryImage5 from '../../assets/images/gallery/gallery-image5.jpg'
+import galleryImage6 from '../../assets/images/gallery/gallery-image6.jpg'
+
+
+const IMAGENS = [
+    galleryHackthon1,
+    galleryHackthon2,
+    galleryHackthon3,
+    galleryImage1,
+    galleryImage2,
+    galleryImage3,
+    galleryImage4,
+    galleryImage5,
+    galleryImage6,
+]
+
+export default IMAGENS;
