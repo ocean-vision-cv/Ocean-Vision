af315b1ec7810166d8975a5620de24d651ba5927
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-02-17 09:41:27 -0100

    fix: events title translation

--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -1,10 +1,13 @@
 import './Events.css'
 import PhotoSlider from '../layout/PhotoSlider'
 
+import { useTranslation } from "react-i18next";
+
 const Events = () => {
+  const {t} = useTranslation()
   return (
     <section className='events'>
-        <h2 className='events-title'>Galeria</h2>
+        <h2 className='events-title'>{t("events-title")}</h2>
         <PhotoSlider/>
     </section>
   )
