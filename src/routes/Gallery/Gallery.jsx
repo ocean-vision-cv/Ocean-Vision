58ecbdca5d9ff7df6d341452e10998a7a03d6cf4
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-08-19 18:51:11 -0100

    feat: add photo description

--- src/routes/Gallery/Gallery.jsx
+++ src/routes/Gallery/Gallery.jsx
@@ -4,10 +4,33 @@ import storage from "../../firebase/FirebaseConfig";
 import Footer from "../../components/pages/Footer/Footer";
 import "./Gallery.css";
 
+const eventList = {
+  "100FO2":["Feira Oportunidades 2", "Mindelo 2023"],
+  "101DD":["Demo day BCVD#4", "Praia 2023"],
+  "102DOL":["Dia Oceano", "Mindelo, Laginha 2023"],
+  "103DORP":["Dia Oceano", "Ribeira das Patas 2023"],
+  "104DORTB":["Dia Oceano", "Replica Torre de Belem 2023"],
+  "105OYS":["Ocean Youth Summit", "Dinamarca, Aarhus 2023"],
+  "106VPR":["Visita PR São Tomé e Príncipe", "ISECMAR 2023"],
+  "107FAPN":["Feira Azul", "Porto Novo 2023"],
+  "108TOCM":["The Ocean Race", "Mindelo StopOver 2023"],
+  "109FO1":["Feira Oportunidades 1", "Mindelo 2022"],
+  "110OWH":["Ocean Week Hackathon", "Mindelo 2022"],
+}
+
 const Gallery = () => {
   const [imagesUrls, setImagesUrls] = useState([]);
   const [selectImage, setSelectImage] = useState(null);
   const [selectImageDescription, setSelectImageDescription] = useState([]);
+  
+  function GetFileNameByURL(url) {
+    var parts = url.split("/");
+    return parts[parts.length - 1]
+      .split("?")[0]
+      .split("galeria%2F")[1]
+      .split(".")[0]
+      .split("%")[0];
+  }
 
   useEffect(() => {
     async function getData() {
@@ -16,7 +39,8 @@ const Gallery = () => {
       const urlPromises = result.items.map((imageRef) =>
         getDownloadURL(imageRef)
       );
-      setImagesUrls(await Promise.all(urlPromises));
+      var urlList = await Promise.all(urlPromises);
+      setImagesUrls(urlList);
     }
     getData();
   }, []);
@@ -44,13 +68,9 @@ const Gallery = () => {
       </p>
       <div className="gallery-container">
         {imagesUrls.map((link) => {
-          var parts = link.split("/");
-          var file_name = parts[parts.length - 1]
-            .split("?")[0]
-            .split("galeria%2F")[1]
-            .split(".")[0];
-          var photoDescription = file_name.split("-");
-
+          // var photoDescription = eventList[GetFileNameByURL(link)]
+          var fileName = GetFileNameByURL(link)
+          var photoDescription = eventList[fileName]
           return (
             <div className="gallery-photo-container" key={link}>
               {!selectImage && (
