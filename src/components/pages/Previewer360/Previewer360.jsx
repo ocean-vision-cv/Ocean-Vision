40e3bc27351b914b3801bfca0ebf098252ad1c7b
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-08-20 18:25:27 -0100

    fix: class name issues

--- src/components/pages/Previewer360/Previewer360.jsx
+++ src/components/pages/Previewer360/Previewer360.jsx
@@ -28,16 +28,15 @@ const Previewer360 = () => {
         </button>
       </div>
 
-      <div class="custom-shape-divider-top-1692195853">
+      <div className="custom-shape-divider-top-1692195853">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
+          
         >
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
           ></path>
         </svg>
       </div>
@@ -50,7 +49,7 @@ const Previewer360 = () => {
       >
         <source src={videoLink}></source>
       </video>
-      <div class="custom-shape-divider-bottom-1692196263">
+      <div className="custom-shape-divider-bottom-1692196263">
         <svg
           data-name="Layer 1"
           xmlns="http://www.w3.org/2000/svg"
@@ -59,7 +58,6 @@ const Previewer360 = () => {
         >
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
           ></path>
         </svg>
       </div>
